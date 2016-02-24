552a3ff88a8a9a635d02fee1a8ece8c4101d5538
andrea <andrea@onixpro.local>
2016-02-24 13:03:13 -0400

    Editing person done and improving UI

--- app/js/peopleCtrl.js
+++ app/js/peopleCtrl.js
@@ -4,7 +4,9 @@ peopleRegister
     var url = FBURL + "/people";
     var fireRef = new Firebase(url);
     $scope.people = $firebaseArray(fireRef);
-
+    $scope.isEditing = false;
+    $scope.editedPerson = null;
+    $scope.newPerson = {};
     $scope.createNewPerson = function() {
       $scope.people.$add({
         name: $scope.person.name,
@@ -18,4 +20,24 @@ peopleRegister
     $scope.deletePerson = function(person) {
       $scope.people.$remove(person);
     }
+
+    $scope.editPerson = function(person) {
+      $scope.editedPerson = person
+      $scope.originalPerson = angular.extend({}, $scope.editedPerson);
+    }
+
+
+    $scope.saveEditing = function(person) {
+      $scope.editedPerson = null;
+      $scope.people.$save(person);
+    };
+
+    $scope.cancelEditing = function(person) {
+      person.name = $scope.originalPerson.name;
+      person.lastName = $scope.originalPerson.lastName;
+      person.email = $scope.originalPerson.email;
+      person.phone = $scope.originalPerson.phone;
+      $scope.saveEditing(person);
+    };
+
   }]);
